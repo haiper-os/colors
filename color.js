18e3b85e1cf395b57cd4cf4b239fdb24b309e483
banyudu <banyudu@gmail.com>
2024-02-24 20:36:12 +0800

    init

---
+++ color.js
@@ -0,0 +1,120 @@
+export const gray = {
+  50: 'var(--hp-gray-50, #f6f6f9)',
+  100: 'var(--hp-gray-100, #eaeaf0)',
+  200: 'var(--hp-gray-200, #d1d1db)',
+  300: 'var(--hp-gray-300, #a9a9bc)',
+  400: 'var(--hp-gray-400, #8a8aa3)',
+  500: 'var(--hp-gray-500, #6c6c89)',
+  600: 'var(--hp-gray-600, #55556d)',
+  700: 'var(--hp-gray-700, #3f3f50)',
+  800: 'var(--hp-gray-800, #282833)',
+  900: 'var(--hp-gray-900, #121217)',
+};
+
+export const white = {
+  50: 'var(--hp-white-50, rgba(255, 255, 255, 0.05))',
+  60: 'var(--hp-white-50, rgba(255, 255, 255, 0.06))',
+  100: 'var(--hp-white-100, rgba(255, 255, 255, 0.1))',
+  200: 'var(--hp-white-200, rgba(255, 255, 255, 0.2))',
+  300: 'var(--hp-white-300, rgba(255, 255, 255, 0.3))',
+  400: 'var(--hp-white-400, rgba(255, 255, 255, 0.4))',
+  500: 'var(--hp-white-500, rgba(255, 255, 255, 0.5))',
+  600: 'var(--hp-white-600, rgba(255, 255, 255, 0.6))',
+  700: 'var(--hp-white-700, rgba(255, 255, 255, 0.7))',
+  800: 'var(--hp-white-800, rgba(255, 255, 255, 0.8))',
+  900: 'var(--hp-white-900, rgba(255, 255, 255, 0.9))',
+  DEFAULT: '#ffffff',
+};
+
+export const band = {
+  50: 'var(--hp-band-50, #f4f1fd)',
+  100: 'var(--hp-band-100, #e5dfff)',
+  200: 'var(--hp-band-200, #cac0ff)',
+  300: 'var(--hp-band-300, #b0a0ff)',
+  400: 'var(--hp-band-400, #9581ff)',
+  500: 'var(--hp-band-500, #7b61ff)',
+  600: 'var(--hp-band-600, #624ecc)',
+  700: 'var(--hp-band-700, #4a3a99)',
+  800: 'var(--hp-band-800, #312766)',
+  900: 'var(--hp-band-900, #191333)',
+  Disabled: 'var(--hp-band-disabled, rgba(149, 129, 255, 50%))',
+};
+
+export const red = {
+  50: 'var(--hp-red-50, #fef0f4)',
+  100: 'var(--hp-red-100, #fdd8e1)',
+  200: 'var(--hp-red-200, #fbb1c4)',
+  300: 'var(--hp-red-300, #f98ba6)',
+  400: 'var(--hp-red-400, #f76489)',
+  500: 'var(--hp-red-500, #f53d6b)',
+  600: 'var(--hp-red-600, #f3164e)',
+  700: 'var(--hp-red-700, #d50b3e)',
+  800: 'var(--hp-red-800, #af0932)',
+  900: 'var(--hp-red-900, #880727)',
+  Disabled: 'var(--hp-red-disabled, rgba(245, 61, 107, 50%))',
+};
+
+export const yellow = {
+  50: 'var(--hp-yellow-50, #fff9eb)',
+  100: 'var(--hp-yellow-100, #fff3d6)',
+  200: 'var(--hp-yellow-200, #ffe7ad)',
+  300: 'var(--hp-yellow-300, #ffda85)',
+  400: 'var(--hp-yellow-400, #ffce5c)',
+  500: 'var(--hp-yellow-500, #ffc233)',
+  600: 'var(--hp-yellow-600, #faaf00)',
+  700: 'var(--hp-yellow-700, #c28800)',
+  800: 'var(--hp-yellow-800, #8a6100)',
+  900: 'var(--hp-yellow-900, #523900)',
+};
+
+export const green = {
+  50: 'var(--hp-green-50, #eefbf4)',
+  100: 'var(--hp-green-100, #dff8ea)',
+  200: 'var(--hp-green-200, #b2eecc)',
+  300: 'var(--hp-green-300, #84e4ae)',
+  400: 'var(--hp-green-400, #56d990)',
+  500: 'var(--hp-green-500, #2dca72)',
+  600: 'var(--hp-green-600, #26a95f)',
+  700: 'var(--hp-green-700, #1e874c)',
+  800: 'var(--hp-green-800, #17663a)',
+  900: 'var(--hp-green-900, #0f4527)',
+};
+
+export const orange = {
+  50: 'var(--hp-orange-50, #fff2ee)',
+  100: 'var(--hp-orange-100, #ffe8e1)',
+  200: 'var(--hp-orange-200, #ffcdbd)',
+  300: 'var(--hp-orange-300, #ffb399)',
+  400: 'var(--hp-orange-400, #ff9876)',
+  500: 'var(--hp-orange-500, #ff7d52)',
+  600: 'var(--hp-orange-600, #ff571f)',
+  700: 'var(--hp-orange-700, #eb3a00)',
+  800: 'var(--hp-orange-800, #b82e00)',
+  900: 'var(--hp-orange-900, #852100)',
+};
+
+export const pink = {
+  50: 'var(--hp-pink-50, #feecfb)',
+  100: 'var(--hp-pink-100, #fdddf8)',
+  200: 'var(--hp-pink-200, #fcc5f3)',
+  300: 'var(--hp-pink-300, #fa99ea)',
+  400: 'var(--hp-pink-400, #f87ce4)',
+  500: 'var(--hp-pink-500, #f75fde)',
+  600: 'var(--hp-pink-600, #f42ad3)',
+  700: 'var(--hp-pink-700, #db0bb9)',
+  800: 'var(--hp-pink-800, #a5088c)',
+  900: 'var(--hp-pink-900, #70065f)',
+};
+
+export const blue = {
+  50: 'var(--hp-blue-50, #f0faff)',
+  100: 'var(--hp-blue-100, #dbf3ff)',
+  200: 'var(--hp-blue-200, #ade4ff)',
+  300: 'var(--hp-blue-300, #70d1ff)',
+  400: 'var(--hp-blue-400, #38beff)',
+  500: 'var(--hp-blue-500, #00acff)',
+  600: 'var(--hp-blue-600, #0090d6)',
+  700: 'var(--hp-blue-700, #0075ad)',
+  800: 'var(--hp-blue-800, #005985)',
+  900: 'var(--hp-blue-900, #003e5c)',
+};
